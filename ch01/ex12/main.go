9029048cace99e36fa92d9095711e2ee0a3a9a9e
luyang93 <luyangshare93@gmail.com>
2023-02-10 14:18:28 +0800

    ch01, ex12.

--- ch01/ex12/main.go
+++ ch01/ex12/main.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"math"
 	"math/rand"
+	"net/url"
 	"os"
+	"strconv"
 )
 
 // Packages not needed by version in book.
@@ -17,6 +19,8 @@ import (
 	"net/http"
 	"time"
 )
+
+var out io.Writer = os.Stdout
 
 var palette = []color.Color{color.White, color.Black}
 
@@ -33,18 +37,30 @@ func main() {
 
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
+			queryName := "cycles"
+			cycles := 5
+
+			if len(r.URL.RawQuery) != 0 {
+				queries, err := url.ParseQuery(r.URL.RawQuery)
+				if err == nil {
+					if v, ok := queries[queryName]; ok {
+						cycles, _ = strconv.Atoi(v[0])
+					}
+				}
+			}
+
+			lissajous(w, cycles)
 		}
 		http.HandleFunc("/", handler)
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
-	lissajous(os.Stdout)
+	lissajous(out, 5)
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, cycles int) {
 	const (
-		cycles  = 5     // number of complete x oscillator revolutions
+		// cycles  = 5     // number of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
@@ -56,7 +72,7 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
