f177f752bc4313070ff960b7e3651226dbc9642e
luyang93 <luyangshare93@gmail.com>
2023-02-11 23:14:33 +0800

    ch02, tempconv.

---
+++ ch02/tempconv/main.go
@@ -0,0 +1,69 @@
+// Package tempconv performs Celsius and Fahrenheit temperature computations.
+package main
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func main() {
+	ExampleOne()
+	ExampleTwo()
+	ExampleThree()
+}
+
+func ExampleOne() {
+	fmt.Printf("%g\n", BoilingC-FreezingC) // "100" °C
+	boilingF := CToF(BoilingC)
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC)) // "180" °F
+	/*
+		fmt.Printf("%g\n", boilingF-FreezingC)       // compile error: type mismatch
+	*/
+
+	// Output:
+	// 100
+	// 180
+}
+
+func ExampleTwo() {
+	c := FToC(212.0)
+	fmt.Println(c.String()) // "100°C"
+	fmt.Printf("%v\n", c)   // "100°C"; no need to call String explicitly
+	fmt.Printf("%s\n", c)   // "100°C"
+	fmt.Println(c)          // "100°C"
+	fmt.Printf("%g\n", c)   // "100"; does not call String
+	fmt.Println(float64(c)) // "100"; does not call String
+
+	// Output:
+	// 100°C
+	// 100°C
+	// 100°C
+	// 100°C
+	// 100
+	// 100
+}
+
+func ExampleThree() {
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0) // "true"
+	fmt.Println(f >= 0) // "true"
+	/*
+		fmt.Println(c == f)          // compile error: type mismatch
+	*/
+	fmt.Println(c == Celsius(f)) // "true"!
+}
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}
